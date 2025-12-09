2c67d39515334608ec6c1596fb0485c3ada40742
aleksandrazaghloul <aleksandra.zaghloul@panintelligence.com>
2025-12-09 16:39:54 +0000

    allow empty outputs when resources not created

--- outputs.tf
+++ outputs.tf
@@ -37,99 +37,99 @@ output "efs_lambda_security_group" {
 
 
 output "dashboard_internal_target_group" {
-  value = module.dashboard[0].internal_target_group
+  value = try(module.dashboard[0].internal_target_group, null)
 }
 
 output "dashboard_external_target_group" {
-  value = module.dashboard[0].external_listener_rule
+  value = try(module.dashboard[0].external_listener_rule, null)
 }
 
 output "dashboard_task_definition" {
-  value = module.dashboard[0].task_definition
+  value = try(module.dashboard[0].task_definition, null)
 }
 
 output "dashboard_service" {
-  value = module.dashboard[0].service
+  value = try(module.dashboard[0].service, null)
 }
 
 output "dashboard_external_listener_rule" {
-  value = module.dashboard[0].external_listener_rule
+  value = try(module.dashboard[0].external_listener_rule, null)
 }
 
 output "dashboard_internal_listener_rule" {
-  value = module.dashboard[0].internal_listener_rule
+  value = try(module.dashboard[0].internal_listener_rule, null)
 }
 
 output "dashboard_security_group" {
-  value = module.dashboard[0].main_security_group
+  value = try(module.dashboard[0].main_security_group, null)
 }
 
 
 
 
 output "renderer_target_group" {
-  value = module.renderer[0].listener_rule
+  value = try(module.renderer[0].listener_rule, null)
 }
 
 output "renderer_task_definition" {
-  value = module.renderer[0].task_definition
+  value = try(module.renderer[0].task_definition, null)
 }
 
 output "renderer_service" {
-  value = module.renderer[0].service
+  value = try(module.renderer[0].service, null)
 }
 
 output "renderer_listener_rule" {
-  value = module.renderer[0].listener_rule
+  value = try(module.renderer[0].listener_rule, null)
 }
 
 output "renderer_security_group" {
-  value = module.renderer[0].main_security_group
+  value = try(module.renderer[0].main_security_group, null)
 }
 
 
 
 
 output "scheduler_target_group" {
-  value = module.scheduler[0].listener_rule
+  value = try(module.scheduler[0].listener_rule, null)
 }
 
 output "scheduler_task_definition" {
-  value = module.scheduler[0].task_definition
+  value = try(module.scheduler[0].task_definition, null)
 }
 
 output "scheduler_service" {
-  value = module.scheduler[0].service
+  value = try(module.scheduler[0].service, null)
 }
 
 output "scheduler_listener_rule" {
-  value = module.scheduler[0].listener_rule
+  value = try(module.scheduler[0].listener_rule, null)
 }
 
 output "scheduler_security_group" {
-  value = module.scheduler[0].main_security_group
+  value = try(module.scheduler[0].main_security_group, null)
 }
 
 
 
 
 output "pirana_target_group" {
-  value = module.pirana[0].listener_rule
+  value = try(module.pirana[0].listener_rule, null)
 }
 
 output "pirana_task_definition" {
-  value = module.pirana[0].task_definition
+  value = try(module.pirana[0].task_definition, null)
 }
 
 output "pirana_service" {
-  value = module.pirana[0].service
+  value = try(module.pirana[0].service, null)
 }
 
 output "pirana_listener_rule" {
-  value = module.pirana[0].listener_rule
+  value = try(module.pirana[0].listener_rule, null)
 }
 
 output "pirana_security_group" {
-  value = module.pirana[0].main_security_group
+  value = try(module.pirana[0].main_security_group, null)
 }
 
